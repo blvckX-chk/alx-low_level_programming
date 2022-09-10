03855fef4be7fc54aeebdf8bf6637f45b75a2bfe
blvckX-chk <jeangodwinyallou@gmail.com>
2022-09-09 17:31:23 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,9 +11,6 @@
 int main(void)
 {
 
-		char displayed[52];
-
-			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-				puts(displayed);
-					return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
