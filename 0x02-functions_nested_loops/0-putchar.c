0a4d8300a445422e4c11bbb968b4ec1c841d4e8f
blvckX-chk <60050505+blvckX-chk@users.noreply.github.com>
2022-10-20 23:10:26 +0200

    putchar main.h update commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar and new line
+ *
+ * Return: returns 0
+ **/
+int main(void)
 {
+	char c[8] = "_putchar";
+	int i = 0;
 
-	// Get the character to be written
-	char ch = '_putchar';
-
-	// Write the Character to stdout
-	putchar(ch);
-
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
