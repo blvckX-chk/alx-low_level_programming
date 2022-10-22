b79b3b223ee00d507cc242e52390fad9bd026fad
blvckX-chk <60050505+blvckX-chk@users.noreply.github.com>
2022-10-22 03:42:34 +0200

    3 4 5 6 7 add commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
