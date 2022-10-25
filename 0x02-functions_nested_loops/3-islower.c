740824bc2848983ab7943df0ee9599ce915c4c52
blvckX-chk <60050505+blvckX-chk@users.noreply.github.com>
2022-10-26 00:37:20 +0200

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
 *_islower - controls if a character is in lowercase
-*@c: character to be verified
+* character to be verified
 *Return: return 0 or 1
 */
 int _islower(int c)
